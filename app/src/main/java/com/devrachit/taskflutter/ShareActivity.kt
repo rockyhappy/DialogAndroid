efb2025fba52718d9f3faaa0a7fe2255a8f50e86
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2024-06-03 19:38:27 +0530

    Added pip mode

--- app/src/main/java/com/devrachit/taskflutter/ShareActivity.kt
+++ app/src/main/java/com/devrachit/taskflutter/ShareActivity.kt
@@ -1,28 +1,31 @@
 package com.devrachit.taskflutter
 
 import android.app.AlertDialog
+import android.app.PictureInPictureParams
 import android.content.DialogInterface
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Rational
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 
 class ShareActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        // Set an empty view or use a simple layout with a TextView
-        setContentView(R.layout.activity_share)
-
-        // Handle the incoming intent
-        val intent = intent
-        if (Intent.ACTION_SEND == intent.action && intent.type != null) {
-            if ("text/plain" == intent.type) {
-                handleSendText(intent) // Handle text being sent
-            }
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        // Set an empty view or use a simple layout with a TextView
+//        setContentView(R.layout.activity_share)
+//
+//        // Handle the incoming intent
+//        val intent = intent
+//        if (Intent.ACTION_SEND == intent.action && intent.type != null) {
+//            if ("text/plain" == intent.type) {
+//                handleSendText(intent) // Handle text being sent
+//            }
+//        }
+//    }
 
     fun handleSendText(intent: Intent) {
         val sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
@@ -39,6 +42,38 @@ class ShareActivity : AppCompatActivity() {
                     })
                 .setIcon(R.drawable.ic_launcher_foreground)
                 .show()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            enterPipMode()
+        }
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            enterPipMode()
+        }
+    }
+
+    private fun enterPipMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val aspectRatio = Rational(16, 9)
+            val pipBuilder = PictureInPictureParams.Builder()
+            pipBuilder.setAspectRatio(aspectRatio)
+            enterPictureInPictureMode(pipBuilder.build())
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            enterPipMode()
         }
     }
 }
